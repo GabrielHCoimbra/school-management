2019ed7dc046dd775f499122efaa29bc3025fa88
GabrielHCoimbra <gabriel.coimbra.viana@gmail.com>
2022-09-08 16:08:13 -0300

    second commit

--- index.php
+++ index.php
@@ -4,9 +4,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Gestão Escola</title>
 </head>
 <body>
-    
+    <header>
+        <h1>Gestão Escolar</h1>
+        <ul>
+            <li><a href="">Cadastrar Alunos</a></li>
+            <li><a href="">Cadastrar Turmas</a></li>
+            <li><a href="">Consultar Notas</a></li>
+            <li><a href="">Lançar Notas</a></li>
+            <li><a href="">Lançar Chamadas</a></li>
+            <li><a href="">Consultar Alunos</a></li>
+            <li><a href="">Consultar Turmas</a></li>
+        </ul>
+    </header>
 </body>
 </html>
